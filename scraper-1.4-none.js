303b811faf30fd3e3be5150ce0f2d6254a1edb61
Marek <mpietrewicz@gmail.com>
2017-09-29 20:22:43 +0200

    Add working scraper version 1.4-none new

--- scraper-1.4-none.js
+++ scraper-1.4-none.js
@@ -13,50 +13,54 @@ server.listen(port, function(request, response) {
   // var page = new WebPage();
   var page = require('webpage').create();
   var url = "https://allegro.pl/kategoria/opony-samochodowe-letnie-99202?string=RE002%20205%2F55%2FR15&order=m&bmatch=base-relevance-floki-5-nga-hc-aut-1-1-0901";
-  var data = "";
+  var data = "none";
 
-  page.open(url, function(status) {
-    console.log('Start scraping url ' +url);
-    if (status === "success") {
-      page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js", function() {
-        // var data = page.evaluate(function(){
-        //
-        //   var section = $("h2:contains('lista ofert')" )[0].parentNode.parentNode;
-        //   var adsList = section.getElementsByTagName('article');
-        //   var numberOfAds = section.getElementsByTagName('article').length;
-        //   var json = [];
-        //
-        //   for (var i = 0; i < adsList.length; i++) {
-        //     json.push({
-        //       title:  adsList[i].getElementsByTagName('h2')[0].textContent,
-        //       url:    adsList[i].getElementsByTagName('a')[0].href,
-        //       price:  ''
-        //     });
-        //   }
-        //
-        //   return json;
-        // });
+  response.statusCode = 200;
+  response.write(JSON.stringify(data));
+  response.close();
 
-        // Rather than console logging, write the data back as a
-        // response to the user
-        //
-        console.log('Finded items: ' +data.length);
-
-        response.statusCode = 200;
-        response.write(JSON.stringify(data));
-        response.close();
-
-        // We want to keep phantom open for more requests, so
-        // instead of exiting - we close the webpage and we're
-        // ready for more requests.
-        //
-        // phantom.exit();
-
-        page.close();
-      });
-    } else {
-      console.log("HTTP status " +status);
-      phantom.exit(1);
-    }
-  });
+  // page.open(url, function(status) {
+  //   console.log('Start scraping url ' +url);
+  //   if (status === "success") {
+  //     page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js", function() {
+  //       // var data = page.evaluate(function(){
+  //       //
+  //       //   var section = $("h2:contains('lista ofert')" )[0].parentNode.parentNode;
+  //       //   var adsList = section.getElementsByTagName('article');
+  //       //   var numberOfAds = section.getElementsByTagName('article').length;
+  //       //   var json = [];
+  //       //
+  //       //   for (var i = 0; i < adsList.length; i++) {
+  //       //     json.push({
+  //       //       title:  adsList[i].getElementsByTagName('h2')[0].textContent,
+  //       //       url:    adsList[i].getElementsByTagName('a')[0].href,
+  //       //       price:  ''
+  //       //     });
+  //       //   }
+  //       //
+  //       //   return json;
+  //       // });
+  //
+  //       // Rather than console logging, write the data back as a
+  //       // response to the user
+  //       //
+  //       console.log('Finded items: ' +data.length);
+  //
+  //       response.statusCode = 200;
+  //       response.write(JSON.stringify(data));
+  //       response.close();
+  //
+  //       // We want to keep phantom open for more requests, so
+  //       // instead of exiting - we close the webpage and we're
+  //       // ready for more requests.
+  //       //
+  //       // phantom.exit();
+  //
+  //       page.close();
+  //     });
+  //   } else {
+  //     console.log("HTTP status " +status);
+  //     phantom.exit(1);
+  //   }
+  // });
 });
